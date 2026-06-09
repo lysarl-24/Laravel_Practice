ba89642106ed797713db98815f74e1c318255951
lysarl-24 <ly.sarl@student.passerellesnumeriques.org>
2026-06-09 19:45:12 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ImployeeController;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UserController;
+use App\Models\Product;
 use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
@@ -41,3 +43,15 @@ Route::get('/students', [StudentController::class, 'index'])->name('students.ind
 Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
 Route::get('/students/show', [StudentController::class, 'show'])->name('students.show');
 Route::post('/students/store', [StudentController::class, 'store'])->name('students.store');
+Route::get('/students/{id}', [StudentController::class, 'edit'])->name('student.edit');
+Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');
+Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
+
+// Product Routes
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::get('/products/show', [ProductController::class, 'show'])->name('products.show');
+Route::post('/products/store', [ProductController::class, 'store'])->name('propducts.store');
+Route::get('/products/{id}', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
